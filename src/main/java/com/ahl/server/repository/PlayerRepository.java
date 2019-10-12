12a19ab4e85dd7923fb37f87226be8368b01ea05
kumble <kumble.k@zohocorp.com>
2019-10-13 02:16:19 +0530

    replaced mongo repo with crud repo for player

--- src/main/java/com/ahl/server/repository/PlayerRepository.java
+++ src/main/java/com/ahl/server/repository/PlayerRepository.java
@@ -5,10 +5,11 @@ import com.ahl.server.entity.Player;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface PlayerRepository extends MongoRepository<Player, ObjectId> {
+public interface PlayerRepository extends CrudRepository<Player, String> {
 
   public Player findFirstByEmailId(String emailId);
 
